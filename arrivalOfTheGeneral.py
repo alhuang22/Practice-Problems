7cf6866db892484f0a7e86b9e881c79b446eda6a
Alex Huang <alexxh0098@gmail.com>
2020-05-01 15:26:59 -0700

    arrival of the general problem

---
+++ arrivalOfTheGeneral.py
@@ -0,0 +1,10 @@
+num = int(input())
+line = list(map(int,input().split()))
+big = line.index(max(line))
+small = len(line)- 1 -line[::-1].index(min(line))
+print(small)
+print(big)
+swaps = big + (len(line) - small) - 1
+if small < big:
+    swaps -= 1
+print(swaps)
