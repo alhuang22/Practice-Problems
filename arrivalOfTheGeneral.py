8866ba8c8d7d1c29456f9738339435777798c373
Alex Huang <alexxh0098@gmail.com>
2020-05-01 15:28:18 -0700

    arrival fo the general problem

--- arrivalOfTheGeneral.py
+++ arrivalOfTheGeneral.py
@@ -2,8 +2,6 @@ num = int(input())
 line = list(map(int,input().split()))
 big = line.index(max(line))
 small = len(line)- 1 -line[::-1].index(min(line))
-print(small)
-print(big)
 swaps = big + (len(line) - small) - 1
 if small < big:
     swaps -= 1
